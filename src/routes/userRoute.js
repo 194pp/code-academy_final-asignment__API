7ac5463f6fe762847d54b3a9d0ff73c0ae923bec
194pp <zilvinas111@gmail.com>
2022-01-05 00:52:46 +0200

    Cleaned up userRoute.js

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -3,10 +3,12 @@ const router = Router();
 const {usersController} = require('../controllers');
 const {authenticateToken} = require('../helpers/auth/jwtHelper');
 
+router.use(authenticateToken);
+
 // USE: /users
-router.get('/', authenticateToken, usersController.getAll);
-router.get('/:_id', authenticateToken, usersController.getOne);
-router.delete('/', authenticateToken, usersController.deleteOne);
-router.put('/', authenticateToken, usersController.modifyOne);
+router.get('/', usersController.getAll);
+router.get('/:_id', usersController.getOne);
+router.delete('/', usersController.deleteOne);
+router.put('/', usersController.modifyOne);
 
 module.exports = router;
